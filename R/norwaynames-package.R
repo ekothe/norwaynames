52e43ed9630993831e56035fee252c4d3b6ed158
Emily <pc.miss@gmail.com>
2019-02-02 13:37:33 +1100

    intial commit

---
+++ R/norwaynames-package.R
@@ -0,0 +1,32 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' All Norwegian names.
+#'
+#' Full baby name data provided by Statistics Norway. This includes all names
+#' with at least 4 uses.
+#'
+#' @format A data frame with five variables: \code{first_name}, \code{year},
+#'   \code{Percent} (the proportion of births in that year with that name),
+#'   \code{Count} (the number of births in that year with that name)and \code{Sex}.
+"all"
+
+#' Girls.
+#'
+#' Female baby name data provided by Statistics Norway. This includes all names
+#' with at least 4 uses.
+#'
+#' @format A data frame with five variables: \code{first_name}, \code{year},
+#'   \code{Percent} (the proportion of births in that year with that name),
+#'   \code{Count} (the number of births in that year with that name)and \code{Sex}.
+"girls"
+
+#' Boys.
+#'
+#' Male baby name data provided by Statistics Norway. This includes all names
+#' with at least 4 uses.
+#'
+#' @format A data frame with five variables: \code{first_name}, \code{year},
+#'   \code{Percent} (the proportion of births in that year with that name),
+#'   \code{Count} (the number of births in that year with that name)and \code{Sex}.
+"boys"
---
+++ README.md
@@ -0,0 +1,16 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+norwaynames
+===========
+
+The norwaynames package provides the following datasets - `allnames` this dataset includes the sex, count, and proportion of births for each first name from 1880 - 2017. The data set includes all names with more than 4 instances in any given year. Names have an NA count or percentage if they were not in use in that year. Source: Statistics Norway <https://www.ssb.no/en/statbank/table/10467/>
+
+    library(norwaynames)
+    head(norwaynames)
+
+Installation
+============
+
+You can install the most recent version of norwaynames from github:
+
+`install_github("ekothe/norwaynames")`
